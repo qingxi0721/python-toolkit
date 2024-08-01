cbaa706e4272a4889d1cdbec2117682b0edcfb04
qingxi <1917320383@qq.com>
2024-08-01 17:19:42 +0800

    feat: add data assert class

---
+++ src/module/data_assert/DataAssert.py
@@ -0,0 +1,41 @@
+import json
+import os
+import uuid
+from typing import Literal
+
+
+class DataAssert:
+    def __init__(self):
+        self.data_assert_set = []
+
+    # 添加数据校验
+    def add_assert(self, result: Literal['success', 'failed', 'skipped'], content: str) -> uuid.UUID:
+        data_id = uuid.uuid4()
+        self.data_assert_set.append({"result": result, "content": content, "id": data_id})
+        return data_id
+
+    # 删除数据校验
+    def delete_assert(self, assert_id: uuid.UUID) -> bool:
+        for data in self.data_assert_set:
+            if data["id"] == assert_id:
+                self.data_assert_set.remove(data)
+                return True
+        return False
+
+    # 获取数据校验
+    def get_assert(self) -> list[dict[str, str]]:
+        return self.data_assert_set
+
+    # 将数据校验写入文件
+    def write_assert_file(self, path: str) -> str:
+        try:
+            with open(path, 'w', encoding="utf-8") as file:
+                result = [
+                    {"result": data["result"], "content": data["content"]}
+                    for data in self.data_assert_set
+                ]
+                json.dump(result, file, ensure_ascii=False)
+            return os.path.abspath(path)
+
+        except Exception as e:
+            print(f"数据写入失败：{e}")
