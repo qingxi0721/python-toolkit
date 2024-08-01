34e237501c078042182d1247d22b4705befcad76
qingxi <1917320383@qq.com>
2024-08-01 17:16:23 +0800

    refactor: add function type

--- src/module/reading_config/ReadingConfig.py
+++ src/module/reading_config/ReadingConfig.py
@@ -1,16 +1,17 @@
 import configparser
 import json
+from typing import Any
 
 import xmltodict
 import yaml
 
 
 class ReadingConfig:
-    def __init__(self, input_file):
+    def __init__(self, input_file: str):
         self.input_file = input_file
         self.file_type = input_file.split('.')[-1]
 
-    def get(self):
+    def get(self) -> Any:
         # 读取ini
         if self.file_type == 'ini':
             return self._get_ini()
@@ -29,7 +30,7 @@ class ReadingConfig:
         else:
             raise ValueError(f"Unsupported file type: {self.file_type}")
 
-    def _get_ini(self):
+    def _get_ini(self) -> dict[str, dict[str, str]]:
         # 创建配置解析器 区分大小写
         config = configparser.RawConfigParser()
         config.optionxform = lambda option: option
@@ -38,14 +39,14 @@ class ReadingConfig:
         # 递归返回字典类型
         return {section: dict(config.items(section)) for section in config.sections()}
 
-    def _get_json(self):
+    def _get_json(self) -> Any:
         with open(self.input_file, 'r') as f:
             return json.load(f)
 
-    def _get_yaml(self):
+    def _get_yaml(self) -> Any:
         with open(self.input_file, 'r') as f:
             return yaml.load(f, Loader=yaml.FullLoader)
 
-    def _get_xml(self):
+    def _get_xml(self) -> Any:
         with open(self.input_file) as f:
             return xmltodict.parse(f.read())
