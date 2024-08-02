60931e8965efc58fc004adbd9117a6c00e9d9581
qingxi <1917320383@qq.com>
2024-08-02 15:31:36 +0800

    feat: reading config:change __init__ method
    Put the parameter('input_file') into the 'get' method to pass.

--- src/module/reading_config/ReadingConfig.py
+++ src/module/reading_config/ReadingConfig.py
@@ -1,5 +1,6 @@
 import configparser
 import json
+import os
 from typing import Any
 
 import xmltodict
@@ -7,46 +8,53 @@ import yaml
 
 
 class ReadingConfig:
-    def __init__(self, input_file: str):
-        self.input_file = input_file
-        self.file_type = input_file.split('.')[-1]
+    def get(self, input_file: str) -> Any:
 
-    def get(self) -> Any:
-        # 读取ini
-        if self.file_type == 'ini':
-            return self._get_ini()
-
-        # 读取json
-        elif self.file_type == 'json':
-            return self._get_json()
-
-        # 读取yaml
-        elif self.file_type == 'yaml':
-            return self._get_yaml()
-
-        # 读取xml
-        elif self.file_type == 'xml':
-            return self._get_xml()
+        try:
+            os.path.exists(input_file)
+        except FileNotFoundError:
+            return None
         else:
-            raise ValueError(f"Unsupported file type: {self.file_type}")
+            file_type = input_file.split('.')[-1]
+            # 读取ini
+            if file_type == 'ini':
+                return self._get_ini(input_file)
 
-    def _get_ini(self) -> dict[str, dict[str, str]]:
+            # 读取json
+            elif file_type == 'json':
+                return self._get_json(input_file)
+
+            # 读取yaml
+            elif file_type == 'yaml':
+                return self._get_yaml(input_file)
+
+            # 读取xml
+            elif file_type == 'xml':
+                return self._get_xml(input_file)
+            else:
+                raise ValueError(f"Unsupported file type: {file_type}")
+
+    @staticmethod
+    def _get_ini(input_file: str) -> dict[str, dict[str, str]]:
         # 创建配置解析器 区分大小写
         config = configparser.RawConfigParser()
         config.optionxform = lambda option: option
-        config.read(self.input_file)
+        config.read(input_file)
 
         # 递归返回字典类型
         return {section: dict(config.items(section)) for section in config.sections()}
 
-    def _get_json(self) -> Any:
-        with open(self.input_file, 'r') as f:
+    @staticmethod
+    def _get_json(input_file: str) -> Any:
+        with open(input_file, 'r') as f:
             return json.load(f)
 
-    def _get_yaml(self) -> Any:
-        with open(self.input_file, 'r') as f:
+    @staticmethod
+    def _get_yaml(input_file: str) -> Any:
+        with open(input_file, 'r') as f:
             return yaml.load(f, Loader=yaml.FullLoader)
 
-    def _get_xml(self) -> Any:
-        with open(self.input_file) as f:
+    @staticmethod
+    def _get_xml(input_file: str) -> Any:
+        with open(input_file) as f:
             return xmltodict.parse(f.read())
