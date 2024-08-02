aea95f0f08e2391df4f247011705e315b2484a64
qingxi <1917320383@qq.com>
2024-08-02 15:36:52 +0800

    test: add global exception test

---
+++ test/exception/example_global_exception.py
@@ -0,0 +1,18 @@
+from src.module.exception.GlobalException import GlobalException
+from src.module.log.Log import Logger
+
+# 创建一个日志处理器
+exception_logger = Logger('DEBUG', 'exception_logger')
+exception_logger.add_stream_handler('stderr', "ERROR")
+
+# 创建一个全局异常处理器
+global_exception_handler = GlobalException(exception_logger)
+# 注册这个处理器
+global_exception_handler.register()
+
+
+def test_global_exception():
+    return 1 / 0
+
+
+test_global_exception()
