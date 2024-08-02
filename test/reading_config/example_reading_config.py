7bbb5dedbf892ba6eedb353c2e515edf612c5b57
qingxi <1917320383@qq.com>
2024-08-02 15:40:42 +0800

    test: add reading config test

---
+++ test/reading_config/example_reading_config.py
@@ -0,0 +1,30 @@
+from src.module.log.Log import Logger
+from src.module.reading_config.ReadingConfig import ReadingConfig
+
+# 日志
+reading_logger = Logger('DEBUG', 'reading_logger')
+reading_logger.add_stream_handler('stdout', "INFO")
+
+# 创建读取配置示例
+config_reader = ReadingConfig()
+
+with reading_logger.logging_context('reading_context'):
+    # 读取ini
+    reading_logger.info('------------------读取ini文件开始------------------')
+    reading_logger.info(config_reader.get('test.ini'))  # 返回字典类型
+    reading_logger.info('------------------读取ini文件结束------------------')
+
+    # 读取json
+    reading_logger.info('------------------读取json文件开始------------------')
+    reading_logger.info(config_reader.get('test.json'))  # 返回字典/字典数组类型
+    reading_logger.info('------------------读取json文件结束------------------')
+
+    # 读取yaml
+    reading_logger.info('------------------读取yaml文件开始------------------')
+    reading_logger.info(config_reader.get('test.yaml'))  # 返回字典类型
+    reading_logger.info('------------------读取yaml文件结束------------------')
+
+    # 读取xml
+    reading_logger.info('------------------读取xml文件开始------------------')
+    reading_logger.info(config_reader.get('test.xml'))  # 返回字典类型
+    reading_logger.info('------------------读取xml文件结束------------------')
