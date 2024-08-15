a501ec2f2ea716de4bc56bd84e4ae746f61c7244
qingxi <1917320383@qq.com>
2024-08-15 17:17:32 +0800

    test(example_reading_config): update reading .xlsx&.csv methods

--- test/reading_config/example_reading_config.py
+++ test/reading_config/example_reading_config.py
@@ -8,23 +8,34 @@ reading_logger.add_stream_handler('stdout', "INFO")
 # 创建读取配置示例
 config_reader = ReadingConfig()
 
-with reading_logger.logging_context('reading_context'):
-    # 读取ini
-    reading_logger.info('------------------读取ini文件开始------------------')
-    reading_logger.info(config_reader.get('test.ini'))  # 返回字典类型
-    reading_logger.info('------------------读取ini文件结束------------------')
+# 读取ini
+reading_logger.info('------------------读取ini文件开始------------------')
+reading_logger.info(config_reader.get('test.ini'))  # 返回字典类型
+reading_logger.info('------------------读取ini文件结束------------------')
 
-    # 读取json
-    reading_logger.info('------------------读取json文件开始------------------')
-    reading_logger.info(config_reader.get('test.json'))  # 返回字典/字典数组类型
-    reading_logger.info('------------------读取json文件结束------------------')
+# 读取json
+reading_logger.info('------------------读取json文件开始------------------')
+reading_logger.info(config_reader.get('test.json'))  # 返回字典/字典数组类型
+reading_logger.info('------------------读取json文件结束------------------')
 
-    # 读取yaml
-    reading_logger.info('------------------读取yaml文件开始------------------')
-    reading_logger.info(config_reader.get('test.yaml'))  # 返回字典类型
-    reading_logger.info('------------------读取yaml文件结束------------------')
+# 读取yaml
+reading_logger.info('------------------读取yaml文件开始------------------')
+reading_logger.info(config_reader.get('test.yaml'))  # 返回字典类型
+reading_logger.info('------------------读取yaml文件结束------------------')
 
-    # 读取xml
-    reading_logger.info('------------------读取xml文件开始------------------')
-    reading_logger.info(config_reader.get('test.xml'))  # 返回字典类型
-    reading_logger.info('------------------读取xml文件结束------------------')
+# 读取xml
+reading_logger.info('------------------读取xml文件开始------------------')
+reading_logger.info(config_reader.get('test.xml'))  # 返回字典类型
+reading_logger.info('------------------读取xml文件结束------------------')
+
+# 读取xlsx
+reading_logger.info('------------------读取xlsx文件开始------------------')
+reading_logger.info(config_reader.get_xlsx('test.xlsx', 'array', 1))  # 返回字典类型
+reading_logger.info(config_reader.get_xlsx('test.xlsx', 'dict', header=1))  # 返回字典类型
+reading_logger.info('------------------读取xlsx文件结束------------------')
+
+# 读取csv
+reading_logger.info('------------------读取csv文件开始------------------')
+reading_logger.info(config_reader.get_csv('test.csv', 'dict'))  # 返回字典类型
+reading_logger.info(config_reader.get_csv('test.csv', 'array', 1))  # 返回字典类型
+reading_logger.info('------------------读取csv文件结束------------------')
